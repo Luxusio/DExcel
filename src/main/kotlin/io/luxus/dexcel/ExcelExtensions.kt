7d6b157c2cea74b385e42abf48647a4fe96423d0
Luxusio <44326048+Luxusio@users.noreply.github.com>
2025-03-01 14:57:51 +0900

    feat: getCell by name

--- src/main/kotlin/io/luxus/dexcel/ExcelExtensions.kt
+++ src/main/kotlin/io/luxus/dexcel/ExcelExtensions.kt
@@ -1,5 +1,7 @@
 package io.luxus.dexcel
 
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.Sheet
 import kotlin.math.pow
 
 /**
@@ -22,4 +24,26 @@ val String.excelColumnIndex: Int get() {
         result += value * 26.0.pow(i.toDouble()).toInt() // 26의 거듭제곱을 적용하여 더합니다.
     }
     return result - 1 // 0-base 인덱스로 변환합니다.
+}
+
+/**
+ * get cell by cell name
+ * @param cellName cell name i.e. A1, B2
+ */
+fun Sheet.getCell(cellName: String): Cell? {
+    val sb = StringBuilder()
+    var i = 0
+    while (i < cellName.length && cellName[i].isLetter()) {
+        sb.append(cellName[i])
+        i++
+    }
+
+    val column = sb.toString()
+    val row = cellName.substring(i)
+
+    if (column.isEmpty() || row.isEmpty()) {
+        return null
+    }
+
+    return this.getRow(row.toInt() - 1)?.getCell(column.excelColumnIndex)
 }
--- src/test/kotlin/io/luxus/dexcel/ExcelExtensionsKtTest.kt
+++ src/test/kotlin/io/luxus/dexcel/ExcelExtensionsKtTest.kt
@@ -7,7 +7,9 @@ import io.kotest.data.headers
 import io.kotest.data.row
 import io.kotest.data.table
 import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 /**
  * @author kjkim
@@ -38,12 +40,52 @@ class ExcelExtensionsKtTest: DescribeSpec({
             row("A1"), // number
             row("A1A"), // number
         ).forAll { invalidInput ->
-            it("should throw IllegalArgumentException") {
+            it("$invalidInput should throw IllegalArgumentException") {
                 val exception = shouldThrow<IllegalArgumentException> {
                     invalidInput.excelColumnIndex
                 }
                 exception.message shouldNotBe null
                 exception.message!! shouldBeEqual "invalid input: $invalidInput"
+            }
+        }
+    }
+
+    describe("Sheet.getCell") {
+        it("should return cell") {
+            // given
+            val workbook = XSSFWorkbook()
+            val sheet = workbook.createSheet("sheet1")
+            val row = sheet.createRow(0)
+            val cell = row.createCell(0)
+            cell.setCellValue("test")
+
+            // when
+            val result = sheet.getCell("A1")
+
+            // then
+            result shouldNotBe null
+            result!!.stringCellValue shouldBeEqual "test"
+        }
+
+        table(
+            headers("nullCellName"),
+            row(""),
+            row("A1"),
+            row("A2"),
+            row("B123123123"),
+            row("A123123123"),
+            row("ZZZZZZZ12333445"),
+        ).forAll { nullCellName ->
+            it("$nullCellName should return null") {
+                // given
+                val workbook = XSSFWorkbook()
+                val sheet = workbook.createSheet("sheet1")
+
+                // when
+                val result = sheet.getCell(nullCellName)
+
+                // then
+                result shouldBe null
             }
         }
     }
